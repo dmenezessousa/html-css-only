6c71b557dd1d94a14cf45193b27d554a83824a60
Diego Sousa <khesanh@diegos-mbp.myfiosgateway.com>
2022-08-04 17:30:42 -0400

    Ask user for the right Input

--- script.js
+++ script.js
@@ -31,3 +31,52 @@ btnToggle2.onclick = function () {
       break;
   }
 };
+
+function greetUser() {
+  const max_count = 5;
+  const user = prompt("What is your name?");
+
+  if (user == "") {
+    alert("Please enter your name");
+    if (max_count > 0) {
+      greetUser();
+    }
+  } else if (isNaN(user) == false) {
+    alert("Please enter your name, Only Letters!");
+    if (max_count > 0) {
+      greetUser();
+    }
+  } else {
+    document.write("Hello, and Wecolme " + user + "!");
+  }
+}
+
+function isOldEnough() {
+  const max_count = 5;
+  const age = prompt("How old are you?");
+  if (age == 65) {
+    alert("OK You Can Eat for FREE Enjoy!");
+  } else if (age == "") {
+    alert("You must enter your age!");
+    if (max_count > 0) {
+      isOldEnough();
+    }
+  } else if (age > 65) {
+    alert("You are Too old to Be Eating This Kinda of Food!");
+    if (max_count > 0) {
+      isOldEnough();
+    }
+  } else if (isNaN(age)) {
+    alert("Please enter your age, Only Numbers!");
+    if (max_count > 0) {
+      isOldEnough();
+    }
+  } else {
+    alert(
+      "You are not old enough to Eat for Free! Come Back when you are OLDER!"
+    );
+    if (max_count > 0) {
+      isOldEnough();
+    }
+  }
+}
